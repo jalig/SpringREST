b8c145dbd8324c1d82529a8aa8d186e361e43942
jalig <117361652+jalig@users.noreply.github.com>
2023-02-27 19:48:40 +0400

    done

---
+++ src/main/java/skypro/exception/EmployeeExceptionHandler.java
@@ -0,0 +1,20 @@
+package skypro.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<MessageForException> handleException(
+            EmployeeException employeeException) {
+        MessageForException message = new MessageForException();
+        message.setMessage(employeeException.getMessage());
+
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/skypro/exception/MessageForException.java
@@ -0,0 +1,16 @@
+package skypro.exception;
+
+public class MessageForException {
+    private String message;
+
+    public MessageForException() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
